74d6eaf896cf40036dfb2c3266e1427af24d6ff7
pepilica <57628520+pepilica@users.noreply.github.com>
2020-02-24 20:10:23 +0300

    Тренировки в полёте

--- base.py
+++ base.py
@@ -10,5 +10,10 @@ def colon(title):
     return render_template('base.html', title=title)
 
 
+@app.route('/training/<prof>')
+def training(prof):
+    return render_template('train.html', name=prof)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
