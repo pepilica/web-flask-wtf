ae1a8963673e11e63c0036b21fff9bee0ab98a97
pepilica <57628520+pepilica@users.noreply.github.com>
2020-03-01 15:49:51 +0300

    Двойная защита

--- base.py
+++ base.py
@@ -1,7 +1,19 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, SubmitField
+from wtforms.validators import DataRequired
 
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+
+class LoginForm(FlaskForm):
+    username = StringField('ID астронавта', validators=[DataRequired()])
+    password = PasswordField('Пароль астронавта', validators=[DataRequired()])
+    username_captain = StringField('ID капитана', validators=[DataRequired()])
+    password_captain = PasswordField('Пароль капитана', validators=[DataRequired()])
+    submit = SubmitField('Войти')
 
 
 @app.route('/<title>')
@@ -24,6 +36,14 @@ def list_prof(param):
 @app.route('/auto_answer', methods=['POST', 'GET'])
 def answer():
     return render_template('auto_answer.html', title='Регистрация')
+
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return '''Login successful'''
+    return render_template('login.html', title='Авторизация', form=form)
 
 
 if __name__ == '__main__':
