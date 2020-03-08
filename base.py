3a10d415c5cfa94f290f3b7b6ec41b7efbdf70ea
pepilica <57628520+pepilica@users.noreply.github.com>
2020-03-08 14:22:13 +0300

    Цвет каюты

--- base.py
+++ base.py
@@ -51,5 +51,10 @@ def distribution(people):
     return render_template('distribution.html', people=people.replace(', ', ',').split(','))
 
 
+@app.route('/table/<sex>/<int:age>')
+def room_decor(sex, age):
+    return render_template('table.html', sex=sex, age=age)
+
+
 if __name__ == '__main__':
     app.run(port=8080, host='127.0.0.1')
